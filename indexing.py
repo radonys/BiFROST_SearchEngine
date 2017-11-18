008192709f4f53d3bbeb4a9c3ae082af9a3b5ea7
Yash Srivastava <yashsrivastava3005@gmail.com>
2017-11-18 16:58:10 +0530

    Added TF-IDF.

--- indexing.py
+++ indexing.py
@@ -3,15 +3,19 @@ import importlib
 import sys
 import collections
 import json
+import math
 
 import text_extract as TE
 
 def index(words,filepath):
 
+    total_files = 0
+
     for folder,subfolders,files in os.walk(filepath):
         
         for filename in files:
     
+            total_files = total_files + 1
             text_data = TE.extract(os.path.join(os.path.abspath(folder),filename))
 
             if text_data != None:
@@ -24,8 +28,8 @@ def index(words,filepath):
                     document = os.path.join(os.path.abspath(folder),filename)
                     positions = [index for index, value in enumerate(processed) if value == term]
                     words[term][document] = positions
-            break
-        break
+
+    return total_files
 
 def dataload(words,filepath):
 
@@ -37,3 +41,17 @@ def datasave(words):
 
     file = open("index_table.json",'w')
     json.dump(words,file)
+    file.close()
+
+def tfidf(words,words_tfidf,filecount):
+
+    for word in words:
+
+        inverse_ratio = filecount/len(words[word])
+        idf = math.log(inverse_ratio,10)
+
+        for doc in words[word]:
+
+            tf = len(words[word][doc])
+            tf_idf = (1 + math.log(tf))*idf
+            words_tfidf[word][doc] = (tf,idf,tf_idf)
--- main.py
+++ main.py
@@ -9,27 +9,37 @@ import indexing
 def main(args):
 
     words = collections.defaultdict(dict)
-    words_idf = dict()
+    words_tfidf = collections.defaultdict(dict)
+    filecount = 0
 
-    if args.json_file==None and args.data_dir==None:
+    if args.data_json_file==None and args.data_dir==None and args.tfidf_json_file==None:
         
         sys.exit("No Data Available")
 
-    elif args.json_file!=None:
-        indexing.dataload(words,args.json_file)
+    elif args.tfidf_json_file!=None:
+        indexing.dataload(words_tfidf,args.tfidf_json_file)
+
+    elif args.data_json_file!=None:
+        indexing.dataload(words,args.data_json_file)
+        indexing.tfidf(words,words_tfidf,filecount)
+        indexing.datasave(words_tfidf)
 
     elif args.data_dir!=None:
-        indexing.index(words,args.data_dir)
+        filecount = indexing.index(words,args.data_dir)
+        indexing.tfidf(words,words_tfidf,filecount)
+        indexing.datasave(words_tfidf)
 
-    indexing.datasave(words)
-
-
+    if words_tfidf:
+        
+        print "Success"
+    
 def parse_arguments(argv):
     
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data_dir', type=str, help='Path to the data directory', default=None)
-    parser.add_argument('--json_file', type=str, help='Path to the JSON file', default=None) 
+    parser.add_argument('--data_json_file', type=str, help='Path to the JSON file', default=None)
+    parser.add_argument('--tfidf_json_file', type=str, help='Path to the TF-IDF JSON file', default=None)
 
     return parser.parse_args(argv)
 
